66beb2ebdfc0d3f22823f6265c9ed4a8d1f2c1f7
fanwh <fanwh@guahao.com>
2018-12-17 10:46:52 +0800

    xxxxx

--- boot-application/src/main/java/cn/edu/nbu/light/interceptor/MyInterceptor.java
+++ boot-application/src/main/java/cn/edu/nbu/light/interceptor/MyInterceptor.java
@@ -1,10 +1,4 @@
-/*
- * Copyright (c) 2001-2018 GuaHao.com Corporation Limited. All rights reserved. 
- * This software is the confidential and proprietary information of GuaHao Company. 
- * ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only 
- * in accordance with the terms of the license agreement you entered into with GuaHao.com.
- */
+
 package cn.edu.nbu.light.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
